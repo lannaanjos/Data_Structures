21a65c0e4c1de9f93b70da8b771713cf36bd8645
Lanna <lannadosanjoscosta@gmail.com>
2026-03-04 15:47:30 -0300

    Repo Init

---
+++ hanoiTower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void torreHanoi(int n, char origem, char destino, char auxiliar){
+    if (n == 1)
+        return;
+
+    torreHanoi(n-1, origem, auxiliar, destino);
+    printf("Disco %d, %c -> %c", n, origem, destino);
+    torreHanoi(n-1, auxiliar, destino, origem);
+}
+
+int main(void) {
+
+    return 0;
+}
