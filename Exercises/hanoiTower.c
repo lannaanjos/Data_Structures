8004d6510be4f543e7c31f862eba76a9ff24d817
Lanna <lannadosanjoscosta@gmail.com>
2026-03-04 15:52:36 -0300

    HanoiTower Finished

--- Exercises/hanoiTower.c
+++ Exercises/hanoiTower.c
@@ -5,11 +5,11 @@ void torreHanoi(int n, char origem, char destino, char auxiliar){
         return;
 
     torreHanoi(n-1, origem, auxiliar, destino);
-    printf("Disco %d, %c -> %c", n, origem, destino);
+    printf("Disco %d, %c -> %c\n", n, origem, destino);
     torreHanoi(n-1, auxiliar, destino, origem);
 }
 
 int main(void) {
-
+    torreHanoi(5, 'A', 'B', 'C');
     return 0;
 }
