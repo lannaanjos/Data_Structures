135b784ff76a21f8fe09bf2b21a2ea5b62d83ae8
Lanna <lannadosanjoscosta@gmail.com>
2026-04-06 11:45:11 -0300

    feat: add node creation function

--- Data_Structures/binary_tree.h
+++ Data_Structures/binary_tree.h
@@ -3,14 +3,29 @@
 #include <string.h>
 
 typedef struct No{
-    int valor; // valor do nó
-    int nivel; // nível do nó
+    int valor;              // valor do nó
+    int nivel;              // nível do nó
     struct No* esquerda;    // próximo nó se for menor
     struct No* direita;     // próximo nó se for maior
 }
 
-void criarNo(){
+// Criação do nó
+No* criarNo(int valor){
+    No* novoNo = (No*)malloc(sizeof(No)); // cria do tamanho de um nó
 
+    if (novoNo == NULL){
+        // stderr = standard error, que é seprado do stdout (saída normal do programa)
+        // serve para manter mensagens de erro separadas da saída normal
+        fprintf(stderr, "Erro na alocação\n");
+        exit(1); // encerra o programa imediatamente com código de saída 1 (código de erro)
+    }
+
+    // atribuição dos campos
+    novoNo->valor = valor;
+    novoNo->esquerda = NULL;
+    novoNo->direita = NULL;
+
+    return novoNo;
 }
 
 void inserirNo(){
